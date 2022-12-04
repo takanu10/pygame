a5c3cac99a0d86771fea675cd3a610e1e88b2e8a
unknown <darrencheng10@gmail.com>
2022-12-04 02:09:35 -0800

    found way to link flask page to pygame button

--- screens/game_over.py
+++ screens/game_over.py
@@ -1,7 +1,8 @@
 import pygame
 from screens.base_screen import BaseScreen
 from components.text_box import TextBox
-
+import app
+import os
 
 class GameOverScreen(BaseScreen):
     def __init__(self, *args, **kwargs):
@@ -14,25 +15,37 @@ class GameOverScreen(BaseScreen):
         self.button2 = TextBox(
             (200, 100), "Quit", color=(255,255,255), bgcolor=(207, 126, 21)
         )
+        self.button3 = TextBox(
+            (200, 100), "High Scores", color=(255,255,255), bgcolor=(207, 126, 21)
+        )
         # self.button1.rect.topleft = (200, 400)
         # self.button2.rect.topleft = (500, 400)
-        self.sprites.add(self.title, self.button1, self.button2)
+        self.sprites.add(self.title, self.button1, self.button2, self.button3)
 
     def draw(self):
         self.window.fill((21, 68, 207))
-        self.title.rect.x = 215
+        self.title.rect.x = 210
         self.title.rect.y = 150
         self.button1.rect.x = 70
         self.button1.rect.y = 300
         self.button2.rect.x = 330
         self.button2.rect.y = 300
+        self.button3.rect.x = 200
+        self.button3.rect.y = 440
         self.sprites.draw(self.window)
 
     def manage_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
+            #restarts game
             if self.button1.rect.collidepoint(event.pos):
                 self.running = False
-                self.next_screen = "welcome"
+                self.next_screen = "game"
+            #quits game
             elif self.button2.rect.collidepoint(event.pos):
                 self.running = False
                 self.next_screen = False
+            #goes to flask high scores 
+            elif self.button3.rect.collidepoint(event.pos):
+                os.system("start \"\" http://127.0.0.1:5000/")
+                # app.homepage()
+                # print("app is running")
