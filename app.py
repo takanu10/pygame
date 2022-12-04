1d16028d8e62636f87211a901ff349fde4397975
unknown <darrencheng10@gmail.com>
2022-12-04 04:18:11 -0800

    added details flask page

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 from screens.game import GameScreen
 import json
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 
-# def read_json(filename="./database/database.json"):
-#     """read + updates json file """
-with open("./database/database.json", 'r') as file:
-    data = json.load(file)
+def read_json(filename="./database/database.json"):
+    """read + updates json file """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        return data
         # return data
         # file_load["Time"].append(data)
     # total = f"{data} sec"
@@ -20,8 +21,14 @@ with open("./database/database.json", 'r') as file:
 @app.route("/")
 def homepage():
     #json.dumps makes it not a dict; want it to be a dict 
-    my_dict = {"title":"BRACE", "json_data":data, "count": 0, "len": len(data)}
+    my_dict = {"title":"BRACE", "json_data":read_json()}
     return render_template("homepage.html", **my_dict)
+
+@app.route("/details", methods=['GET', 'POST'])
+def details():
+    #json.dumps makes it not a dict; want it to be a dict 
+    my_dict = {"title": "BRACE"}
+    return render_template("details.html", **my_dict)
 
     
 
